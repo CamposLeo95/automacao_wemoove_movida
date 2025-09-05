690712d0fbe265c2b22d96124f07362aa12e91c7
Leonardo Campos <leocampos.995@gmail.com>
2025-09-05 19:12:06 -0300

    comment utils-get-instance

--- src/utils/get-instance.ts
+++ src/utils/get-instance.ts
@@ -1,9 +1,10 @@
 import { StartBrowser } from "../service/StartBrowser.service";
 
 
-export default async function getWsEndpoint() {
-	const browserInstance = await StartBrowser.getInstance();
-	const ws = await browserInstance.getWsEndpoint();
 
-	return ws;
-}
+// export default async function getWsEndpoint() {
+// 	const browserInstance = await StartBrowser.launchBrowser();
+// 	const ws = await browserInstance.getWsEndpoint();
+
+// 	return ws;
+// }
